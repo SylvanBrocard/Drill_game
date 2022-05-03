c6d095dd0048b1f6cebe8c89d58766227720c5b4
Odiano PAQUET <odiano@free.fr>
2022-05-03 13:56:54 +0200

    changement vizualizer class

--- Drill_game/vizualizer.py
+++ Drill_game/vizualizer.py
@@ -1,4 +1,25 @@
-from abc import ABC
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-class Prospect(ABC):
-    pass
+class Vizualizer():
+    '''Classe du vizualiser afin de voir des graphiques de performances'''
+    #prospect_perf matrice 
+    def __init__(self,prospect_perf:np.ndarray) -> None:
+        self.x, self.y = prospect_perf.shape
+        self.prospect_perf = prospect_perf
+        pass
+
+    def graph_3d(self):
+        ax = plt.axes(projection='3d') # Création d'un objet "axe 3D"
+        for l in range(self.x):
+            for c in range(self.y):
+                ax.scatter(l, c, self.prospect_perf[l,c], c=self.prospect_perf[l,c])
+
+
+    def graph_2D(self):
+
+np.matrice = [[1,2,3],[2,5,4]]
+viz = Vizualizer(matrice)
+
+viz.graph_3d
