b8cd09fa9a22498705e5f66a18b6ec0b004e6997
SylvanBrocard <sylvan.brocard@gmail.com>
2022-05-03 11:37:50 +0200

    moved code

---
+++ Drill_game/prospect.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+class Prospect(ABC):
+    pass
+
+class UniformProspect(Prospect):
+    pass
+
+class EGreedyProspect(Prospect):
+    pass
+
+class UCB1Prospect(Prospect):
+    pass
