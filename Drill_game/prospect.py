514c1cac39e0db6d26ae57e437a48990bfba99f9
SylvanBrocard <sylvan.brocard@gmail.com>
2022-05-03 12:26:25 +0200

    uniform prospecter

--- Drill_game/prospect.py
+++ Drill_game/prospect.py
@@ -1,10 +1,33 @@
 from abc import ABC
+from statistics import mean
+import numpy as np
+
+from prospect import Terrain
 
 class Prospect(ABC):
-    pass
+    def __init__(self, trials:int, terrain:Terrain) -> None:
+        super().__init__()
+        self.trials = trials
+        self.rewards = []
+        self.terrain = terrain
+        self.knowledge = np.empty_like(terrain.grid)
+
+    def prospect_once(self) -> None:
+        pass
+
+    def prospect(self) -> None:
+        for _ in range(self.trials):
+            self.prospect_once()
+
+    def get_total_reward(self) -> int:
+        return sum(self.rewards)
 
 class UniformProspect(Prospect):
-    pass
+    def prospect_once(self) -> None:
+        x, y = self.terrain.get_random_coordinate()
+        self.rewards.append(((x,y),self.terrain.get_reward(x,y)))
+        tile_draws = [reward for loc, reward in self.rewards if loc == (x,y)]
+        self.knowledge[x,y] = mean(tile_draws)
 
 class EGreedyProspect(Prospect):
     pass
